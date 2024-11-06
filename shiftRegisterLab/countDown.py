230cb62ad88ebc11224e80e502da1bec46eb3cf4
IsaacFenster <isaackof48@gmail.com>
2024-11-06 12:57:48 -0600

    v3

--- shiftRegisterLab/countDown.py
+++ shiftRegisterLab/countDown.py
@@ -1,5 +1,5 @@
-# import RPi.GPIO as GPIO
-# import time
+import RPi.GPIO as GPIO
+import time
 
 switchPin = 16
 aPin = 12
@@ -8,26 +8,55 @@ cPin = 14
 dPin = 15
 ePin = 17
 fPin = 18
-gpin = 19
+gPin = 19
 
-myList = [aPin, bPin, cPin]
+pinList = [aPin, bPin, cPin, dPin, ePin, fPin, gPin]
 
-for i in myList:
-    print(i)
 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.cleanup()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
 
-# GPIO.setup(switchPin, GPIO.IN)
-# GPIO.setup(ledPin, GPIO.OUT)
-# GPIO.setup(buzzPin, GPIO.OUT)
+GPIO.setup(switchPin, GPIO.IN)
+for i in pinList:
+    GPIO.setup(i, GPIO.OUT) 
 
-# while True:
-#     if(GPIO.input(switchPin)):
-#         GPIO.output(ledPin,True)
-#         GPIO.output(buzzPin,True)
-#         time.sleep(1)
-#     else:
-#         GPIO.output(ledPin,False)
-#         GPIO.output(buzzPin,False)
+# counts down from 9->0 when switch is pushed
+def countdown():
+    # Displaying 9
+    GPIO.output(aPin, True)
+    GPIO.output(fPin, True)
+    GPIO.output(gPin, True)
+    GPIO.output(bPin, True)
+    GPIO.output(cPin, True)
+    GPIO.output(dPin, True)
+    GPIO.output(ePin, False)
+
+    time.sleep(1)
+
+    # Displaying 8
+    GPIO.output(ePin, True)
+
+    time.sleep(1)
+
+    # Displaying 7
+    GPIO.output(fPin, True)
+    GPIO.output(gPin, True)
+    GPIO.output(ePin, True)
+    GPIO.output(dPin, True)
+
+    # Displaying 6
+    
+
+
+while True:
+    if(GPIO.input(switchPin)):
+        countdown()
+    else:
+        for i in pinList:
+            GPIO.output(i,False)
+       
+
+
+    
+    
